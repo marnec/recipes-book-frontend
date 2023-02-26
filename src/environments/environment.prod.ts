d796ef4f0192f582509d258d3d862e0e5f750807
marnec <marnec@ryzenbeast.localdomain>
2023-02-26 14:35:03 +0000

    change env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,6 @@
+import { firebase } from '../../../firebase.config'
+
 export const environment = {
-  firebase: {
-    projectId: 'recipes-ac036',
-    appId: '1:320224370121:web:52fd8c7dbca4550c049c18',
-    storageBucket: 'recipes-ac036.appspot.com',
-    locationId: 'europe-west',
-    apiKey: 'AIzaSyBKCgzrncsKImMBbPq7x2EBKi5SaMA3Lks',
-    authDomain: 'recipes-ac036.firebaseapp.com',
-    messagingSenderId: '320224370121',
-    measurementId: 'G-ND7VKS1HCE',
-  },
+  firebase,
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,18 +1,10 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+import { firebase } from '../../../firebase.config'
 
 export const environment = {
-  firebase: {
-    projectId: 'recipes-ac036',
-    appId: '1:320224370121:web:52fd8c7dbca4550c049c18',
-    storageBucket: 'recipes-ac036.appspot.com',
-    locationId: 'europe-west',
-    apiKey: 'AIzaSyBKCgzrncsKImMBbPq7x2EBKi5SaMA3Lks',
-    authDomain: 'recipes-ac036.firebaseapp.com',
-    messagingSenderId: '320224370121',
-    measurementId: 'G-ND7VKS1HCE',
-  },
+  firebase,
   production: false,
   api: 'http://localhost:3000'
 };
